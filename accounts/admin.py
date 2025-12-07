a2b20d0ac7b50cffc9acc2308a8cf23061f7f995
Zaid <zaid.alnemash@eng.ku.edu.kw>
2025-12-08 01:54:58 +0300

    Register models in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, DoctorProfile, PatientProfile, Availability, Appointment
+from .models import User, PatientProfile, DoctorProfile, Appointment, Availability, MedicalReport
 
 admin.site.register(User)
-admin.site.register(DoctorProfile)
 admin.site.register(PatientProfile)
-admin.site.register(Availability)
+admin.site.register(DoctorProfile)
 admin.site.register(Appointment)
+admin.site.register(Availability)
+admin.site.register(MedicalReport)
